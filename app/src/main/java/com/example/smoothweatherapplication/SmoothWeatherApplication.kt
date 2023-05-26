53b45c0062a3826260cdf62fd886dfe75ed52ed4
Veronika <dyoma.veronika@gmail.com>
2023-05-26 11:18:38 +0200

    add init project structure and init files

---
+++ app/src/main/java/com/example/smoothweatherapplication/SmoothWeatherApplication.kt
@@ -0,0 +1,7 @@
+package com.example.smoothweatherapplication
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class SmoothWeatherApplication : Application()
---
+++ app/src/main/java/com/example/smoothweatherapplication/presentation/ui/theme/Color.kt
@@ -0,0 +1,6 @@
+package com.example.smoothweatherapplication.presentation.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val WarmWhite = Color(0xFFFFFFFE)
+val LightBlue = Color(0xFFC4E2FE)
